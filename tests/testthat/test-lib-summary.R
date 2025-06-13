769d3a343258a7056d9a626a601c216371356dd5
jbjurling <bjurlingjosefin@gmail.com>
2025-06-13 11:55:12 +0200

    added parameter "sizes"

--- tests/testthat/test-lib-summary.R
+++ tests/testthat/test-lib-summary.R
@@ -1,12 +1,9 @@
-test_that("lib_summary returns expected results", {
-  res <- lib_summary()
-  expect_s3_class(res, "data.frame")
-  expect_equal(ncol(res), 2)
-  expect_equal(names(res), c("Library", "n_packages"))
-  expect_type(res$Library, "character")
-  expect_type(res$n_packages, "integer")
+test_that("lib_summary fails appropriately", {
+  expect_error(lib_summary(sizes = "foo"), "not interpretable as logical")
 })
 
-test_that("lib_summary fails appropriately", {
-  expect_error(lib_summary("foo"), "unused argument")
+test_that("sizes argument works", {
+  res <- lib_summary(sizes = TRUE)
+  expect_equal(names(res), c("Library", "n_packages", "lib_size"))
+  expect_type(res$lib_size, "double")
 })
